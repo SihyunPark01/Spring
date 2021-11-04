46025e3354a89b12f0ad110bc7a2a1c7c8c4b9d2
maro0226 <maro0226@naver.com>
2021-11-04 17:45:04 +0900

    상품등록

--- Kmarket/src/main/java/kr/co/kmarket/controller/MainController.java
+++ Kmarket/src/main/java/kr/co/kmarket/controller/MainController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import kr.co.kmarket.service.MainService;
 import kr.co.kmarket.vo.CategoriesVo;
@@ -19,15 +20,19 @@ public class MainController {
 	@GetMapping(value= {"/","/index"})
 	public String index(Model model) {
 	
-		List<CategoriesVo> cates = service.selectCategories();
-		model.addAttribute("cates", cates);
+		//이 방법 하지말고
+		//List<CategoriesVo> cates = service.selectCategories();
+		//model.addAttribute("cates", cates);
 		
 		return "/index";
 	}
 	
+	@ResponseBody
 	@GetMapping("/getCategories")
-	public void getCategories() {
+	public List<CategoriesVo> getCategories() {
 		
+		List<CategoriesVo> cates = service.selectCategories();
+		return cates; // /getCategories 링크 들어가서 확인해봐
 		
 	}
 	
